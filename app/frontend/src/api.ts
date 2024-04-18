c5140322297d0edce23ea2067634017c3b4b59b2
Tima Ulezko <ulezko.2015@mail.ru>
2024-04-18 10:06:48 +0300

    add env-dependent server url

--- app/frontend/src/api.ts
+++ app/frontend/src/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {store} from '@/redux';
 import {userActions} from '@/redux/userSlice';
 
-export const baseUrl = '/api';
+export const baseUrl = process.env.NODE_ENV === 'production' ? 'https://185.233.187.57:8081/api' : '/api';
 
 axios.interceptors.request.use(config => {
   config.headers['Authorization'] = store.getState().user ? 'Bearer ' + store.getState().user?.jwt : null;
