9c48fa3b1827345c9ba774d8122a19793a9b4d6f
IchinichiQ <IchinichiGit@gmail.com>
2024-05-01 23:43:25 +0300

    Fix variable name

--- app/frontend/src/api.ts
+++ app/frontend/src/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {store} from '@/redux';
 import {userActions} from '@/redux/userSlice';
 
-export const baseUrl = process.env.NODE_ENV === 'production' ? process.env.VITE_BACKEND_ADDRESS : '/api';
+export const baseUrl = process.env.NODE_ENV === 'production' ? import.meta.env.VITE_BACKEND_ADDRESS : '/api';
 
 axios.interceptors.request.use(config => {
   config.headers['Authorization'] = store.getState().user ? 'Bearer ' + store.getState().user?.jwt : null;
