374a3670b6a39befb3e6f9f9ae84d224136c42cd
Tima Ulezko <66557561+lezko@users.noreply.github.com>
2024-06-20 23:40:44 +0300

    COWER-82: Manage bookings page (#61)

--- app/frontend/src/routes/checkout-page.tsx
+++ app/frontend/src/routes/checkout-page.tsx
@@ -7,29 +7,45 @@ import { useState } from "react";
 import { ErrorText } from "@/styles/styles";
 import { baseUrl } from "@/api";
 import ym from "react-yandex-metrika";
+import { getTimeDelta } from "@/utils/getTimeDelta";
+import { useAuthorizedUser } from "@/redux/userSlice";
+import { UserRole } from "@/types/User";
+import { useNavigate } from "react-router-dom";
+import styled, { keyframes } from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
+
+const kf = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const LoaderWrapper = styled("div")`
+  display: flex;
+  animation: ${kf} 1s linear infinite;
+`;
 
 export const CheckoutPage = () => {
+  const user = useAuthorizedUser();
   const { order } = useAppSelector((state) => state.order);
   const [errors, setErrors] = useState<string[]>([]);
+  const [loading, setLoading] = useState(false);
 
   const { address, date, place, timeFrom, timeTo, price, seatId } = order!;
 
   const [isStudent, setIsStudent] = useState(false);
 
-  const spFrom = timeFrom.split(":");
-  const spTo = timeTo.split(":");
-  const hourFrom = +spFrom[0];
-  const minuteFrom = +spFrom[1];
-  const hourTo = +spTo[0];
-  const minuteTo = +spTo[1];
-  const tFrom = hourFrom * 60 + minuteFrom;
-  const tTo = hourTo * 60 + minuteTo;
-  const timeDelta = (tTo - tFrom) / 60;
-
+  const timeDelta = getTimeDelta(timeFrom, timeTo);
   const initialPrice = Math.floor(price * timeDelta);
   const finalPrice = isStudent ? Math.floor(initialPrice * 0.9) : initialPrice;
 
+  const navigate = useNavigate();
   const handleSubmit = () => {
+    setLoading(true);
     setErrors([]);
     axios
       .post(`${baseUrl}/v1/bookings`, {
@@ -40,7 +56,11 @@ export const CheckoutPage = () => {
         applyDiscount: isStudent,
       })
       .then((res) => {
-        window.open(res.data.booking.paymentUrl, "_self");
+        if (user.role === UserRole.ADMIN) {
+          navigate("/payment-result");
+        } else {
+          window.open(res.data.booking.paymentUrl, "_self");
+        }
         ym("reachGoal", "create_order");
       })
       .catch((e) => {
@@ -49,7 +69,8 @@ export const CheckoutPage = () => {
         } else {
           setErrors(["Не удалось авторизоваться"]);
         }
-      });
+      })
+      .finally(() => setLoading(false));
   };
 
   return (
@@ -73,10 +94,22 @@ export const CheckoutPage = () => {
             onChange={(e) => setIsStudent(e.target.checked)}
           />
           <label htmlFor="student">
-            Я студент (необходимо предоставить студ. билет или зачётную книжку)
+            {user.role === UserRole.ADMIN
+              ? "Студенческая скидка (необходимо предоставить студ. билет или зачётную книжку)"
+              : "Я студент (необходимо предоставить студ. билет или зачётную книжку)"}
           </label>
         </Flex>
-        <Button onClick={handleSubmit}>Оплатить заказ</Button>
+
+        <Flex align="center" gap={20}>
+          <Button onClick={handleSubmit}>
+            {user.role === UserRole.ADMIN ? "Создать заказ" : "Оплатить заказ"}
+          </Button>
+          {loading && (
+            <LoaderWrapper>
+              <FontAwesomeIcon icon={faCircleNotch} />
+            </LoaderWrapper>
+          )}
+        </Flex>
         {errors.map((error, index) => (
           <ErrorText key={index}>{error}</ErrorText>
         ))}
