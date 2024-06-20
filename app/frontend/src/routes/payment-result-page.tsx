ab1467c46c2d121f9204f6410839b1ec97a00350
Timofey Ulezko <ulezko.2015@mail.ru>
2024-06-20 23:25:03 +0300

    add manage bookings page

--- app/frontend/src/routes/payment-result-page.tsx
+++ app/frontend/src/routes/payment-result-page.tsx
@@ -1,13 +1,18 @@
 import { ToHomeButton } from "@/components/ToHomeButton";
 import { Flex } from "antd";
 import { useNavigate } from "react-router-dom";
+import { useAuthorizedUser } from "@/redux/userSlice";
+import { UserRole } from "@/types/User";
 
 export const PaymentResultPage = () => {
   const navigate = useNavigate();
+  const user = useAuthorizedUser();
 
   return (
     <div style={{ paddingInline: 30 }}>
-      <h1 style={{ marginBlock: 20 }}>Заказ успешно оплачен!</h1>
+      <h1 style={{ marginBlock: 20 }}>
+        Заказ успешно {user.role === UserRole.ADMIN ? "создан" : "оплачен"}!
+      </h1>
       <Flex gap={20}>
         <ToHomeButton />
         <span
@@ -19,7 +24,7 @@ export const PaymentResultPage = () => {
             cursor: "pointer",
           }}
         >
-          Мои заказы
+          {user.role === UserRole.ADMIN ? "Управление заказами" : "Мои заказы"}
         </span>
       </Flex>
     </div>
